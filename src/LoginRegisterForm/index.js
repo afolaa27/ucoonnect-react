f52ac12a202ac875ac4107b4af8b446150710344
Abdulkhaliq <afolaa27@uwgb.edu>
2020-02-27 12:53:00 -0600

    handled select dropdown state

--- src/LoginRegisterForm/index.js
+++ src/LoginRegisterForm/index.js
@@ -13,9 +13,12 @@ class LoginRegisterForm extends Component{
 			email:'',
 			password:'',
 			username:'',
-			action:'login'
-
-
+			age:'',
+			school:'',
+			action:'login',
+			regexEmail: new RegExp('/[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm'),
+			message : '',
+			displayMessage: false
 		}
 	}
 	switchForm =()=>{
@@ -30,34 +33,59 @@ class LoginRegisterForm extends Component{
 			})
 		}
 	}
+	 handleSelectChange = (e, { value, name }) => this.setState({ [name]:value })
 	handleChange=(event)=>{
+
+
+		console.log('event.target.value >>>', event.target.value);
+		console.log('event.target.name >>>', event.target.name);
+
 		this.setState({
 			[event.target.name]: event.target.value
 		})
 	}
+	formValidation=()=>{
+		if (this.state.action == 'register'){
+			if(this.state.email==''|| !this.state.regexEmail.test(this.state.email)){
+
+			}
+		}
+	}
 
 	render(){
+		console.log("this is state >>>>" , this.state);
 			return(
 			<div className='LoginRegisterForm'> 
+
 				<div className='formDiv'>
-				<Form >
+				<Form onSubmit={this.formValidation}>
 				{
 					this.state.action ==='register'
 					?
 					<Form.Field className='input'>
 						<label>Username</label>
-						<input placeholder='User Name'/>
+						<input placeholder='User Name'
+						name='username'
+						value={this.state.username}
+						onChange={this.handleChange}/>
 					</Form.Field>
 					:
 					null
 				}
 					<Form.Field className='input'>
 						<label>Email</label>
-						<input placeholder='johndoe@gmail.com' type='email'/>
+						<input placeholder='johndoe@gmail.com' type='email'
+						name='email'
+						value={this.state.email}
+						onChange={this.handleChange} 
+						/>
 					</Form.Field>
 					<Form.Field className='input'>
 						<label>password</label>
-						<input placeholder='Password' type='password'/>
+						<input placeholder='Password' type='password'
+						name='password'
+						value={this.state.password}
+						onChange={this.handleChange}/>
 					</Form.Field>
 
 				{
@@ -65,18 +93,22 @@ class LoginRegisterForm extends Component{
 					?
 					<Form.Field className='input'>
 						<label>Age</label>
-						<input placeholder='Enter Age' type='number'/>
+						<input placeholder='Enter Age' type='number'
+						name='age'
+						value={this.state.age}
+						onChange={this.handleChange}/>
 
 						<Form.Select  
             				fluid
             				label='School'
+            				name='school'
             				options={schoolKey}
             				placeholder='School'
+            				value={this.state.school}
+							onChange={this.handleSelectChange}
           				/>
 				
 					</Form.Field>
-					
-					
 					:
 					null
 				}
@@ -91,6 +123,7 @@ class LoginRegisterForm extends Component{
 					this.state.action ==='register'
 					?
 					<small> if you have an account Log in <span className="link" onClick={this.switchForm}>here</span>.</small>
+
 					:
 					<small> dont have an account Sign up <span className="link" onClick={this.switchForm}>here</span>!</small>
 				}
